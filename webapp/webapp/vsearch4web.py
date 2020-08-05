6aa97a9d0564c77c1f24895942cfcdbc072772de
21asdfforitacademy <21asdfforitacademy@mail.ru>
2020-08-05 16:14:05 +0200

    Lesson_HF_webapp_05.08.2020.04

--- webapp/webapp/vsearch4web.py
+++ webapp/webapp/vsearch4web.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -12,15 +13,14 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Журналирует веб-запрос и возвращает результаты."""
-    dbcomfig = {'host': '127.0.0.1',
+    dbconfig = {'host': '127.0.0.1',
                 'user': 'vsearch',
                 'password': 'vsearchpasswd',
                 'database': 'vsearchlogDB',}
 
-    import mysql.connector
-
-    conn = mysql.connector.connect(**dbcomfig)
+    conn = mysql.connector.connect(**dbconfig)
     cursor = conn.cursor()
+
     _SQL = """insert into log
               (phrase, letters, ip, browser_string, results)
               value (%s, %s, %s, %s, %s)"""
